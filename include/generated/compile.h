77290d74965103185e3450b5695488bda0603522
W.D <djvoleur@gmail.com>
2015-11-02 00:01:09 -0700

    dts:  Add missing dtsi

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Aug 21 15:01:26 KST 2015"
-#define LINUX_COMPILE_BY "surge"
-#define LINUX_COMPILE_HOST "surge"
-#define LINUX_COMPILER "gcc version 4.9 20140514 (prerelease) (GCC) "
